a182dc545058311f2a5ae807227a9e604807505b
cyberboy1551 <blacy1551@gmail.com>
2018-06-27 13:33:19 -0500

    Added count up timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,16 +6,20 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
     public float time;
+    float _timer;
     public bool startCountDown;
     public Text timeInGame;
 	// Use this for initialization
 	void Start () {
         time = 0.0f;
+        _timer = 0.0f;
         startCountDown = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        _timer += Time.deltaTime;
+        
         if (startCountDown)
         {
             //display time every frame
